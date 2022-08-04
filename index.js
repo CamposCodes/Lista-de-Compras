8b2075473db31a1f723692decbe6ac470fbbf73f
Gabriel Campos Lima Alves <campos.gabriel@estudante.ufjf.br>
2022-08-04 01:47:33 -0300

    referencia entradas, botoes, lista

---
+++ index.js
@@ -0,0 +1,9 @@
+//entradas - inputs
+let entradaNome = document.querySelector("#input__name");
+let entradaNum = document.querySelector("#input__num");
+// botoes - buttons
+let botao = document.querySelector("#button__add");
+let botaoVer = document.querySelector("#button__view");
+// listas - list
+let lista = document.querySelector("#list");
+let list = document.querySelector(".listItem");
