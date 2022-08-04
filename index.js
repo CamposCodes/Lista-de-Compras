34d51ae4313b116e3b32b5158e2cc94d72256525
Gabriel Campos Lima Alves <campos.gabriel@estudante.ufjf.br>
2022-08-04 01:51:18 -0300

    cria função adicionar e comprado

--- index.js
+++ index.js
@@ -7,3 +7,41 @@ let botaoVer = document.querySelector("#button__view");
 // listas - list
 let lista = document.querySelector("#list");
 let list = document.querySelector(".listItem");
+
+botao.addEventListener("click",adicionar)
+
+function adicionar(){
+    let textoItem1 = entradaNome.value;
+    let textoItem2 = entradaNum.value;
+
+    var novoLi = document.createElement("li");
+    novoLi.textContent = textoItem1 + ", " + textoItem2 + "x";
+
+    if( textoItem1 != "" && textoItem2 != ""){
+        if(textoItem2 <= 0){
+            window.alert("ERRO: quantidade inválida !")    
+        }
+        else{
+            lista.appendChild(novoLi);
+        }
+    }
+    else{
+        window.alert("ERRO: Algum dado está incompleto !");
+    }
+
+    novoLi.addEventListener("click",comprar);    
+    
+    entradaNome.value = "";
+    entradaNum.value = "";
+    entradaNome.focus();
+    entradaNum.focus();
+}
+
+function comprar() {
+    if(this.classList.contains("comprado")){
+        this.classList.remove("comprado");
+    }
+    else {
+        this.classList.add("comprado");
+    }
+}
